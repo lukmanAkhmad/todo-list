4672bc88e02775ea4764bf79fa4f1caa78677981
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2024-05-10 08:58:40 +0700

    membuat function createMainSidebar

--- src/uiDashboard.js
+++ src/uiDashboard.js
@@ -1,11 +1,13 @@
 import logoMyTodolist from '/assets/img/playlist-check.svg';
+import plusSign from '/assets/img/plus.svg';
 
 
-
-export function createMainSidebar() {
+export function createContentSidebar() {
     const divSidebar = document.querySelector('#sidebar');
 
     divSidebar.appendChild(createHeaderSidebar());
+    divSidebar.appendChild(createMainSidebar());
+
 }
 
 function createHeaderSidebar(){
@@ -24,4 +26,31 @@ function createHeaderSidebar(){
     headerSidebar.appendChild(paraSidebar);
 
     return headerSidebar
+}
+
+function createMainSidebar(){
+    const mainSidebar = document.createElement('div');
+    mainSidebar.classList.add('main-sidebar');
+
+    const paraMainSidebar = document.createElement('p');
+    paraMainSidebar.classList.add('p-main-sidebar');
+    paraMainSidebar.textContent = 'My List';
+    mainSidebar.appendChild(paraMainSidebar);
+
+    const btnMainSidebar = document.createElement('button');
+    btnMainSidebar.classList.add('btn-main-sidebar');
+    mainSidebar.appendChild(btnMainSidebar);
+
+    const paraBtnMainSidebar = document.createElement('p');
+    paraBtnMainSidebar.classList.add('p-btn-main-sidebar');
+    paraBtnMainSidebar.textContent = 'New';
+    btnMainSidebar.appendChild(paraBtnMainSidebar);
+
+    const imgBtnMainSidebar = new Image();
+    imgBtnMainSidebar.classList.add('img-btn-main-sidebar');
+    imgBtnMainSidebar.src = plusSign;
+    imgBtnMainSidebar.alt = 'plus sign';
+    btnMainSidebar.appendChild(imgBtnMainSidebar);
+
+    return mainSidebar;
 }
