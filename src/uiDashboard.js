dabd3712259702e73f933c56a1c4b41a3bc93bea
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2024-05-12 17:52:07 +0700

    menghapus semua DOM sebelumnya dan menambahkan renderList

--- src/uiDashboard.js
+++ src/uiDashboard.js
@@ -1,107 +1,60 @@
-import logoMyTodolist from '/assets/img/playlist-check.svg';
-import plusSign from '/assets/img/plus.svg';
+// import logoMyTodolist from '/assets/img/playlist-check.svg';
+// import plusSign from '/assets/img/plus.svg';
 import editSign from '/assets/img/playlist-edit.svg';
 import trashSign from '/assets/img/trash-can-outline.svg';
 
+import * as listModule from './list';
 
-export function createContentSidebar() {
-    const divSidebar = document.querySelector('#sidebar');
+const lists = listModule.lists;
 
-    divSidebar.appendChild(createHeaderSidebar());
-    divSidebar.appendChild(createMainSidebar());
-    divSidebar.appendChild(createContainerList());
+function screenControler(){
+    const lists =listModule.lists;
+
+    const headerSidebar = document.querySelector('.header-sidebar');
+    const btnMainSidebar = document.querySelector('.btn-main-sidebar');
+    const containerList = document.querySelector('.container-list');
+    const formContainerList = document.querySelector('.form-container-list');
+    const containerInput = document.querySelector('.container-input');
+    const listInput = document.querySelector('.list-input');
+    const listSpan = document.querySelector('.list-span')    
+
+    const renderList = () => {
+        lists.forEach((list) => {
+            const itemTodolist = document.createElement('li');
+            itemTodolist.classList.add('item-todolist');
+    
+    
+            const paraItemTodolist = document.createElement('p');
+            paraItemTodolist.classList.add('p-item-todolist');
+            paraItemTodolist.textContent = list.name;
+            itemTodolist.appendChild(paraItemTodolist);
+    
+            const containerSvgItemTodolist = document.createElement('div');
+            containerSvgItemTodolist.classList.add('container-svg-item-todolist');
+            itemTodolist.appendChild(containerSvgItemTodolist);
+    
+            const imgEdit = document.createElement('img');
+            imgEdit.classList.add('icon-item-todolist');
+            imgEdit.src = editSign;
+            imgEdit.alt = 'edit sign';
+            containerSvgItemTodolist.appendChild(imgEdit);
+    
+            const imgTrash = document.createElement('img');
+            imgTrash.classList.add('icon-item-todolist');
+            imgTrash.src = trashSign;
+            imgTrash.alt = 'trash sign';
+            containerSvgItemTodolist.appendChild(imgTrash);
+
+            containerList.appendChild(itemTodolist);
+        });
+    }
+
+    btnMainSidebar.addEventListener('click',() => {
+        formContainerList.style.display = 'flex';
+    })
+
+    renderList();
+
 }
 
-function createHeaderSidebar(){
-    const headerSidebar = document.createElement('div');
-    headerSidebar.classList.add('header-sidebar');
-
-    const sidebarIcon = new Image();
-    sidebarIcon.classList.add('header-sidebar-icon');
-    sidebarIcon.alt = 'My Todo List Icon';
-    sidebarIcon.src = logoMyTodolist;
-    headerSidebar.appendChild(sidebarIcon);
-
-    const paraSidebar = document.createElement('p');
-    paraSidebar.classList.add('p-sidebar');
-    paraSidebar.textContent = 'My Todo Lsit';
-    headerSidebar.appendChild(paraSidebar);
-
-    return headerSidebar
-}
-
-function createMainSidebar(){
-    const mainSidebar = document.createElement('div');
-    mainSidebar.classList.add('main-sidebar');
-
-    const paraMainSidebar = document.createElement('p');
-    paraMainSidebar.classList.add('p-main-sidebar');
-    paraMainSidebar.textContent = 'My List';
-    mainSidebar.appendChild(paraMainSidebar);
-
-    const btnMainSidebar = document.createElement('button');
-    btnMainSidebar.classList.add('btn-main-sidebar');
-    mainSidebar.appendChild(btnMainSidebar);
-
-    const paraBtnMainSidebar = document.createElement('p');
-    paraBtnMainSidebar.classList.add('p-btn-main-sidebar');
-    paraBtnMainSidebar.textContent = 'New';
-    btnMainSidebar.appendChild(paraBtnMainSidebar);
-
-    const imgBtnMainSidebar = new Image();
-    imgBtnMainSidebar.classList.add('img-btn-main-sidebar');
-    imgBtnMainSidebar.src = plusSign;
-    imgBtnMainSidebar.alt = 'plus sign';
-    btnMainSidebar.appendChild(imgBtnMainSidebar);
-
-    btnMainSidebar.addEventListener('click', () => {
-        elemenInput.style.display = 'flex';
-    });
-
-    return mainSidebar;
-}
-
-function createContainerList(){
-    const containerList = document.createElement('ul');
-    containerList.classList.add('container-list');
-    containerList.appendChild(createItemTodolist());
-
-    containerList.appendChild(elemenInput);
-
-    return containerList;
-
-};
-
-function createItemTodolist(){
-    const itemTodolist = document.createElement('li');
-    itemTodolist.classList.add('item-todolist');
-
-    const paraItemTodolist = document.createElement('p');
-    paraItemTodolist.classList.add('p-item-todolist');
-    paraItemTodolist.textContent = 'List 1';
-    itemTodolist.appendChild(paraItemTodolist);
-
-    const containerSvgItemTodolist = document.createElement('div');
-    containerSvgItemTodolist.classList.add('container-svg-item-todolist');
-    itemTodolist.appendChild(containerSvgItemTodolist);
-
-    const imgEdit = new Image();
-    imgEdit.classList.add('icon-item-todolist');
-    imgEdit.src = editSign;
-    imgEdit.alt = 'edit sign';
-    containerSvgItemTodolist.appendChild(imgEdit);
-
-    const imgTrash = new Image();
-    imgTrash.classList.add('icon-item-todolist');
-    imgTrash.src = trashSign;
-    imgTrash.alt = 'trash sign';
-    containerSvgItemTodolist.appendChild(imgTrash);
-
-    // buat yang ke 2 dan seterusnya bisa liat contoh restaurant-page
-
-    return itemTodolist;
-}
-
-    const elemenInput = document.createElement('input');
-    elemenInput.setAttribute('id','elemen-input');
-    elemenInput.setAttribute('type','text');
+export { screenControler };
