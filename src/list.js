ca6fc1da75ecd514f8b8221138cb55fade496477
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2024-05-15 21:12:40 +0700

    menambahkan id,task dan completed pada List

--- src/list.js
+++ src/list.js
@@ -1,21 +1,32 @@
 const lists = [
     {
+        id: '1',
         name: 'Beli Buku'
     },
     {
+        id: '2',
         name: 'beli mobil'
     },
 ];
 
 const List = (name) => {
+    let id = Date.now().toString();
+    let tasks = [];
+    let completed = [];
     return {
+        id,
         name,
-    }
-}
+        tasks,
+        completed,
+    };
+};
 
 const createList = (name) => {
-    const list = List(name);
-
+    if(lists.find((lists) => lists.name === name)){
+        const list = List(name);
+    } else {
+        const list = List(name);
+    }
     lists.push(list);
 } 
 
