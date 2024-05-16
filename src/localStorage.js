3ff22e83efe817e67768285d24710b0b346b2b05
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2024-05-16 20:00:04 +0700

    membuat function getIdFromLocalStorage

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,14 +1,19 @@
 const localStoregeKey = 'lists';
 const localStorageIdKey = 'listId';
 
-function saveToLocalStorage(lists, selectedListId){
-    localStorage.setItem(localStoregeKey,JSON.stringify(lists));
+function saveToLocalStorage(lists, selectedListId) {
+    localStorage.setItem(localStoregeKey, JSON.stringify(lists));
     localStorage.setItem(localStorageIdKey, JSON.stringify(selectedListId));
 };
 
-function getFromLocalStorage(){
+function getFromLocalStorage() {
     const data = JSON.parse(localStorage.getItem(localStoregeKey));
     return data;
+};
+
+function getIdFromLocalStorage() {
+    const selectedListId = JSON.parse(localStorage.getItem(localStorageIdKey))
+    return selectedListId;
 }
 
-export { saveToLocalStorage, getFromLocalStorage };
+export { saveToLocalStorage, getFromLocalStorage, getIdFromLocalStorage };
