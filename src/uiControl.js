e29a168d5928c1de7a069739eeecd5c345cb51a1
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-03-23 12:43:08 +0700

    feat: header content

--- src/uiControl.js
+++ src/uiControl.js
@@ -1,11 +1,13 @@
 function renderScreen() {
     const body = document.querySelector("body");
     renderSidebar(body);
+    renderHeaderContent(body);
 };
+
 function renderSidebar(parentNode) {
     const sidebar = document.createElement("div");
     sidebar.setAttribute("id", "sidebar")
-    sidebar.textContent = "im sidebar";
+    sidebar.textContent = "Sidebar";
     sidebarController(sidebar);
 
     parentNode.appendChild(sidebar);
@@ -23,4 +25,11 @@ function sidebarController(parentNode) {
     parentNode.appendChild(sidebarBtn);
 };
 
+function renderHeaderContent(parentNode){
+    const headerContent = document.createElement("div");
+    headerContent.setAttribute("id","header-content");
+    headerContent.textContent = "Header Content";
+
+    parentNode.appendChild(headerContent);
+};
 export { renderScreen };
